c06479d34a601d0eac03da4697893e6d60c69a7c
junjuno5 <j.seo@xiilab.com>
2024-12-05 23:27:54 +0900

    first commit

---
+++ src/main/java/org/springframework/springwebtemplate/SpringWebTemplateApplication.java
@@ -0,0 +1,13 @@
+package org.springframework.springwebtemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebTemplateApplication.class, args);
+    }
+
+}
